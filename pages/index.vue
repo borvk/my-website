52f793327ea0d16f4dcfc8d28d6e78a961290ae6
borvk <hcborak@gmail.com>
2022-12-27 01:57:20 +0700

    chore: change text

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,16 @@
   <section class="md:container md:mx-auto" role="main">
     <Navbar />
     <div class="relative px-6 font-chivo py-20 md:py-48">
-      <div class="py-10">
+      <div class="py-10 text-left">
         Hey there, thank you for visiting my website.
       </div>
-      <div class="text-center">
-        I am currently working as a Web Engineer <a href="https://kh.linkedin.com/company/mangobyte"><span class="text-orange-500 font-semibold">@MangoByte</span></a>. I work with backend and frontend development for web app.
+      <div class="text-left">
+        I am currently working as a Web Engineer <a href="https://kh.linkedin.com/company/mangobyte"><span class="text-orange-500 font-semibold">@MangoByte</span></a>. I work with backend and frontend development for web app, and occassionally I do some web design.
       </div>
-      <div class="py-10">
-       I would love explore mobile development someday, maybe later...
+      <div class="py-10 text-left">
+       I would love to explore mobile development someday, maybe later...
       </div>
-      <div class="py-10">
+      <div class="py-10 text-center">
        Anyway, cheers and happy coding! &ndash; Borak 👨‍💻  
       </div>
       <section class="flex justify-center font-semibold">

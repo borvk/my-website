ef3dcf37d6050750b08faec5b8f8b19efec84ef0
borvk <hcborak@gmail.com>
2022-12-26 21:36:02 +0700

    chore: switched to tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-stone-200 min-h-screen min-w-screen">
     <div class="grid h-screen place-items-center">
-      <div class="p-10 flex-col select-none">
-        <span class="text-xl">  Hi, as you can see... this ain't it. </span>
+      <div class="p-10 flex-col select-none font-chivo">
+        <span class="text-lg font-bold">  Hi, as you can see... this ain't it. </span>
         <div class="text-lg p-3">
         – Borak 👨‍🦲
         </div>
